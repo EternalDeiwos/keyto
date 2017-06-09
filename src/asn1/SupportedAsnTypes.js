9eb7db1a079df6fdbd36840c8c0c137fe3c31079
Greg Linklater <greglink49@gmail.com>
2017-06-09 12:27:33 +0200

    feat(asn1): supported asn1 complex types registry

---
+++ src/asn1/SupportedAsnTypes.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * SupportedAsnTypes
+ * @ignore
+ */
+class SupportedAsnTypes {
+
+  /**
+   * constructor
+   *
+   * @class SupportedAsnTypes
+   *
+   * @description
+   * A registry for supported asn complex field types
+   */
+  constructor () {
+
+  }
+}
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = SupportedAsnTypes
---
+++ src/asn1/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * Module Depdendencies
+ * @ignore
+ */
+const SupportedAsnTypes = require('./SupportedAsnTypes')
+
+/**
+ * supportedAsnTypes
+ * @ignore
+ */
+const supportedAsnTypes = new SupportedAsnTypes()
+
+/**
+ * Export
+ * @ignore
+ */
+module.exports = supportedAsnTypes
