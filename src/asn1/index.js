7dcbaafd651201dea45ed9e9795260701798cb6a
Greg Linklater <greglink49@gmail.com>
2017-06-09 14:24:47 +0200

    feat(asn1): add asn type definitions to registry

--- src/asn1/index.js
+++ src/asn1/index.js
@@ -13,6 +13,30 @@ const SupportedAsnTypes = require('./SupportedAsnTypes')
 const supportedAsnTypes = new SupportedAsnTypes()
 
 /**
+ * General Types
+ * @ignore
+ */
+supportedAsnTypes.define('Version', require('./Version'))
+supportedAsnTypes.define('OtherPrimeInfos', require('./OtherPrimeInfos'))
+supportedAsnTypes.define('AlgorithmIdentifier', require('./AlgorithmIdentifier'))
+supportedAsnTypes.define('PrivateKeyInfo', require('./PrivateKeyInfo'))
+supportedAsnTypes.define('PublicKeyInfo', require('./PublicKeyInfo'))
+
+/**
+ * RSA Types
+ * @ignore
+ */
+supportedAsnTypes.define('RSAPrivateKey', require('./RSAPrivateKey'))
+supportedAsnTypes.define('RSAPublicKey', require('./RSAPublicKey'))
+
+/**
+ * ECDSA Types
+ * @ignore
+ */
+supportedAsnTypes.define('ECDSAPrivateKey', require('./ECDSAPrivateKey'))
+supportedAsnTypes.define('ECDSAPublicKey', require('./ECDSAPublicKey'))
+
+/**
  * Export
  * @ignore
  */
