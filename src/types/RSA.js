c521210f99a28730d8c6d1fe21f60013f1b532e6
Greg Linklater <greglink49@gmail.com>
2017-06-12 00:19:16 +0200

    fix(RSA): cleanup and minor bug fixes

--- src/types/RSA.js
+++ src/types/RSA.js
@@ -10,6 +10,7 @@
  * @ignore
  */
 const KeyType = require('./KeyType')
+const InvalidOperationError = require('../InvalidOperationError')
 const Converter = require('../Converter')
 const asn = require('../asn1')
 
@@ -23,6 +24,10 @@ const asn = require('../asn1')
  * RSA conversion implementation
  */
 class RSA extends KeyType {
+
+  static get kty () {
+    return 'RSA'
+  }
 
   static get oid () {
     return [ 1, 2, 840, 113549, 1, 1, 1 ]
@@ -68,7 +73,18 @@ class RSA extends KeyType {
 
   static fromJwk (jwk) {
     let { n, e, d, p, q, dp, dq, qi, alg } = jwk
-    let data = Converter.convertObject({ n, e, d, p, q, dp, dq, qi }, 'base64url', 'raw')
+
+    let data = {
+      n: Converter.convert(n, 'base64url', 'raw'),
+      e: Converter.convert(e, 'base64url', 'raw'),
+      d: Converter.convert(d, 'base64url', 'raw'),
+      p: Converter.convert(p, 'base64url', 'raw'),
+      q: Converter.convert(q, 'base64url', 'raw'),
+      dp: Converter.convert(dp, 'base64url', 'raw'),
+      dq: Converter.convert(dq, 'base64url', 'raw'),
+      qi: Converter.convert(qi, 'base64url', 'raw'),
+    }
+
     return new RSA(data, { alg })
   }
 
@@ -76,13 +92,33 @@ class RSA extends KeyType {
     return !!this.d
   }
 
+  toPKCS1 () {
+    if (!this.isPrivate) {
+      throw new InvalidOperationError('Cannot export a private key from a public key')
+    }
+
+    let RSAPrivateKey = asn.normalize('RSAPrivateKey')
+
+    let data = { version: 'two-prime' }
+    Object.assign(data, Converter.convertObject(this, 'raw', 'bn'))
+
+    let base64pem = RSAPrivateKey.encode(data, 'der').toString('base64')
+
+    return RSA.formatPem(base64pem, 'RSA PRIVATE')
+  }
+
   toPKCS8 () {
+    if (!this.isPrivate) {
+      throw new InvalidOperationError('Cannot export a private key from a public key')
+    }
+
     Object.defineProperty(this, 'pkcs8', { value: true })
     let PrivateKeyInfo = asn.normalize('PrivateKeyInfo')
     let RSAPrivateKey = asn.normalize('RSAPrivateKey')
 
-    let converted = Converter.convertObject(this, 'raw', 'bn')
-    let privateKey = RSAPrivateKey.encode(converted, 'der')
+    let data = { version: 'two-prime' }
+    Object.assign(data, Converter.convertObject(this, 'raw', 'bn'))
+    let privateKey = RSAPrivateKey.encode(data, 'der')
 
     let base64pem = PrivateKeyInfo.encode({
       version: 'two-prime',
@@ -94,17 +130,6 @@ class RSA extends KeyType {
     }, 'der').toString('base64')
 
     return RSA.formatPem(base64pem, 'PRIVATE')
-  }
-
-  toPKCS1 () {
-    let RSAPrivateKey = asn.normalize('RSAPrivateKey')
-
-    let data = { version: 'two-prime' }
-    Object.assign(data, Converter.convertObject(this, 'raw', 'bn'))
-
-    let base64pem = RSAPrivateKey.encode(data, 'der').toString('base64')
-
-    return RSA.formatPem(base64pem, 'RSA PRIVATE')
   }
 
   toPublicPem () {
@@ -130,17 +155,33 @@ class RSA extends KeyType {
   }
 
   toPrivateJwk () {
+    if (!this.isPrivate) {
+      throw new InvalidOperationError('Cannot export a private key from a public key')
+    }
+
     let { n, e, d, p, q, dp, dq, qi } = this
-    let jwk = Object.assign({ kty: 'RSA' }, Converter.convertObject({ n, e, d, p, q, dp, dq, qi }, 'raw', 'base64url'))
-    delete jwk.version
-    return jwk
+
+    return {
+      kty: 'RSA',
+      n: Converter.convert(n, 'raw', 'base64url'),
+      e: Converter.convert(e, 'raw', 'base64url'),
+      d: Converter.convert(d, 'raw', 'base64url'),
+      p: Converter.convert(p, 'raw', 'base64url'),
+      q: Converter.convert(q, 'raw', 'base64url'),
+      dp: Converter.convert(dp, 'raw', 'base64url'),
+      dq: Converter.convert(dq, 'raw', 'base64url'),
+      qi: Converter.convert(qi, 'raw', 'base64url'),
+    }
   }
 
   toPublicJwk () {
     let { n, e } = this
-    let jwk = Object.assign({ kty: 'RSA' }, Converter.convertObject({ n, e }, 'raw', 'base64url'))
-    delete jwk.version
-    return jwk
+
+    return {
+      kty: 'RSA',
+      n: Converter.convert(n, 'raw', 'base64url'),
+      e: Converter.convert(e, 'raw', 'base64url'),
+    }
   }
 }
 
