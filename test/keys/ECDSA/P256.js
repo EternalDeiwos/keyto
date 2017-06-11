524a773edf3de2e487e126d78050944a1425d0f7
Greg Linklater <greglink49@gmail.com>
2017-06-12 00:24:49 +0200

    test(P-256): unsure if hex is valid

--- test/keys/ECDSA/P256.js
+++ test/keys/ECDSA/P256.js
@@ -18,8 +18,8 @@ N4Q4UA7eR5Uh7BEIzXBGuT/3S9cNXKa6mWLTLIcBxEFVLcx1AVQJKrkFXQ==
  * Hex String
  * @ignore
  */
-const privateHex = `00dedda319da2c8e06b5527df97e6c1ea1632480398d62c1de62991363bd3b5981`
-const publicHex = `046da83747415352f94b24610cef3121636206260a04378438500ede479521ec1108cd7046b93ff74bd70d5ca6ba9962d32c8701c441552dcc750154092ab9055d`
+const privateHex = ``
+const publicHex = ``
 
 /**
  * JWK
