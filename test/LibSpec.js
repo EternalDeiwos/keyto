5257cd8ec2592c93e7e936c1e2a5920ed7dc6bc0
Greg Linklater <greglink49@gmail.com>
2017-06-15 11:54:48 +0200

    test(lib): P-256 tests

--- test/LibSpec.js
+++ test/LibSpec.js
@@ -36,13 +36,19 @@ const {
   },
   ECDSA: {
     K256: {
-      publicPKCS1: ecPublicPKCS1,
-      publicPKCS8: ecPublicPKCS8,
-      publicJwk: ecPublicJwk,
-      privatePKCS8: ecPrivatePKCS8,
-      privatePKCS1: ecPrivatePKCS1,
-      privateJwk: ecPrivateJwk,
-      privateHex: ecPrivateHex
+      publicPKCS8: k256PublicPKCS8,
+      publicJwk: k256PublicJwk,
+      privatePKCS8: k256PrivatePKCS8,
+      privatePKCS1: k256PrivatePKCS1,
+      privateJwk: k256PrivateJwk,
+      privateHex: k256PrivateHex
+    },
+    P256: {
+      publicPKCS8: p256PublicPKCS8,
+      publicJwk: p256PublicJwk,
+      privatePKCS8: p256PrivatePKCS8,
+      privatePKCS1: p256PrivatePKCS1,
+      privateJwk: p256PrivateJwk,
     }
   }
 } = require(path.join(cwd, 'test', 'keys'))
@@ -215,23 +221,25 @@ describe('keyto', () => {
     })
   })
 
-  describe('ECDSA', () => {
+  describe('ECDSA secp256k1', () => {
 
     describe('publicPKCS8', () => {
 
       let key
       before(() => {
-        key = keyto.from(ecPublicPKCS8, 'pem')
+        key = keyto.from(k256PublicPKCS8, 'pem')
       })
 
-      it('should convert to publicPKCS1')
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
 
       it('should convert to publicPKCS8', () => {
-        key.toString('pem', 'public_pkcs8').should.equal(ecPublicPKCS8)
+        key.toString('pem', 'public_pkcs8').should.equal(k256PublicPKCS8)
       })
 
       it('should convert to publicJwk', () => {
-        JSON.stringify(key.toJwk('public'), null, 2).should.equal(ecPublicJwk)
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(k256PublicJwk)
       })
 
       it('should not convert to privatePKCS1', () => {
@@ -255,17 +263,19 @@ describe('keyto', () => {
 
       let key
       before(() => {
-        key = keyto.from(ecPublicJwk, 'jwk')
+        key = keyto.from(k256PublicJwk, 'jwk')
       })
 
-      it('should convert to publicPKCS1')
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
 
       it('should convert to publicPKCS8', () => {
-        key.toString('pem', 'public_pkcs8').should.equal(ecPublicPKCS8)
+        key.toString('pem', 'public_pkcs8').should.equal(k256PublicPKCS8)
       })
 
       it('should convert to publicJwk', () => {
-        JSON.stringify(key.toJwk('public'), null, 2).should.equal(ecPublicJwk)
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(k256PublicJwk)
       })
 
       it('should not convert to privatePKCS1', () => {
@@ -289,33 +299,35 @@ describe('keyto', () => {
 
       let key
       before(() => {
-        key = keyto.from(ecPrivatePKCS1, 'pem')
+        key = keyto.from(k256PrivatePKCS1, 'pem')
       })
 
-      it('should convert to publicPKCS1')
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
 
       it('should convert to publicPKCS8', () => {
-        key.toString('pem', 'public_pkcs8').should.equal(ecPublicPKCS8)
+        key.toString('pem', 'public_pkcs8').should.equal(k256PublicPKCS8)
       })
 
       it('should convert to publicJwk', () => {
-        JSON.stringify(key.toJwk('public'), null, 2).should.equal(ecPublicJwk)
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(k256PublicJwk)
       })
 
       it('should convert to privatePKCS1', () => {
-        key.toString('pem', 'private_pkcs1').should.equal(ecPrivatePKCS1)
+        key.toString('pem', 'private_pkcs1').should.equal(k256PrivatePKCS1)
       })
 
       it('should convert to privatePKCS8', () => {
-        key.toString('pem', 'private_pkcs8').should.equal(ecPrivatePKCS8)
+        key.toString('pem', 'private_pkcs8').should.equal(k256PrivatePKCS8)
       })
 
       it('should convert to privateJwk', () => {
-        JSON.stringify(key.toJwk('private'), null, 2).should.equal(ecPrivateJwk)
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(k256PrivateJwk)
       })
 
       it('should convert to privateHex', () => {
-        key.toString('blk', 'private').should.equal(ecPrivateHex)
+        key.toString('blk', 'private').should.equal(k256PrivateHex)
       })
     })
 
@@ -323,33 +335,35 @@ describe('keyto', () => {
 
       let key
       before(() => {
-        key = keyto.from(ecPrivatePKCS8, 'pem')
+        key = keyto.from(k256PrivatePKCS8, 'pem')
       })
 
-      it('should convert to publicPKCS1')
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
 
       it('should convert to publicPKCS8', () => {
-        key.toString('pem', 'public_pkcs8').should.equal(ecPublicPKCS8)
+        key.toString('pem', 'public_pkcs8').should.equal(k256PublicPKCS8)
       })
 
       it('should convert to publicJwk', () => {
-        JSON.stringify(key.toJwk('public'), null, 2).should.equal(ecPublicJwk)
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(k256PublicJwk)
       })
 
       it('should convert to privatePKCS1', () => {
-        key.toString('pem', 'private_pkcs1').should.equal(ecPrivatePKCS1)
+        key.toString('pem', 'private_pkcs1').should.equal(k256PrivatePKCS1)
       })
 
       it('should convert to privatePKCS8', () => {
-        key.toString('pem', 'private_pkcs8').should.equal(ecPrivatePKCS8)
+        key.toString('pem', 'private_pkcs8').should.equal(k256PrivatePKCS8)
       })
 
       it('should convert to privateJwk', () => {
-        JSON.stringify(key.toJwk('private'), null, 2).should.equal(ecPrivateJwk)
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(k256PrivateJwk)
       })
 
       it('should convert to privateHex', () => {
-        key.toString('blk', 'private').should.equal(ecPrivateHex)
+        key.toString('blk', 'private').should.equal(k256PrivateHex)
       })
     })
 
@@ -357,33 +371,35 @@ describe('keyto', () => {
 
       let key
       before(() => {
-        key = keyto.from(ecPrivateJwk, 'jwk')
+        key = keyto.from(k256PrivateJwk, 'jwk')
       })
 
-      it('should convert to publicPKCS1')
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
 
       it('should convert to publicPKCS8', () => {
-        key.toString('pem', 'public_pkcs8').should.equal(ecPublicPKCS8)
+        key.toString('pem', 'public_pkcs8').should.equal(k256PublicPKCS8)
       })
 
       it('should convert to publicJwk', () => {
-        JSON.stringify(key.toJwk('public'), null, 2).should.equal(ecPublicJwk)
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(k256PublicJwk)
       })
 
       it('should convert to privatePKCS1', () => {
-        key.toString('pem', 'private_pkcs1').should.equal(ecPrivatePKCS1)
+        key.toString('pem', 'private_pkcs1').should.equal(k256PrivatePKCS1)
       })
 
       it('should convert to privatePKCS8', () => {
-        key.toString('pem', 'private_pkcs8').should.equal(ecPrivatePKCS8)
+        key.toString('pem', 'private_pkcs8').should.equal(k256PrivatePKCS8)
       })
 
       it('should convert to privateJwk', () => {
-        JSON.stringify(key.toJwk('private'), null, 2).should.equal(ecPrivateJwk)
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(k256PrivateJwk)
       })
 
       it('should convert to privateHex', () => {
-        key.toString('blk', 'private').should.equal(ecPrivateHex)
+        key.toString('blk', 'private').should.equal(k256PrivateHex)
       })
     })
 
@@ -391,33 +407,198 @@ describe('keyto', () => {
 
       let key
       before(() => {
-        key = keyto.from(ecPrivateHex, 'blk')
+        key = keyto.from(k256PrivateHex, 'blk')
       })
 
-      it('should convert to publicPKCS1')
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
 
       it('should convert to publicPKCS8', () => {
-        key.toString('pem', 'public_pkcs8').should.equal(ecPublicPKCS8)
+        key.toString('pem', 'public_pkcs8').should.equal(k256PublicPKCS8)
       })
 
       it('should convert to publicJwk', () => {
-        JSON.stringify(key.toJwk('public'), null, 2).should.equal(ecPublicJwk)
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(k256PublicJwk)
       })
 
       it('should convert to privatePKCS1', () => {
-        key.toString('pem', 'private_pkcs1').should.equal(ecPrivatePKCS1)
+        key.toString('pem', 'private_pkcs1').should.equal(k256PrivatePKCS1)
       })
 
       it('should convert to privatePKCS8', () => {
-        key.toString('pem', 'private_pkcs8').should.equal(ecPrivatePKCS8)
+        key.toString('pem', 'private_pkcs8').should.equal(k256PrivatePKCS8)
       })
 
       it('should convert to privateJwk', () => {
-        JSON.stringify(key.toJwk('private'), null, 2).should.equal(ecPrivateJwk)
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(k256PrivateJwk)
       })
 
       it('should convert to privateHex', () => {
-        key.toString('blk', 'private').should.equal(ecPrivateHex)
+        key.toString('blk', 'private').should.equal(k256PrivateHex)
+      })
+    })
+  })
+
+  describe('ECDSA prime256v1', () => {
+
+    describe('publicPKCS8', () => {
+
+      let key
+      before(() => {
+        key = keyto.from(p256PublicPKCS8, 'pem')
+      })
+
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
+
+      it('should convert to publicPKCS8', () => {
+        key.toString('pem', 'public_pkcs8').should.equal(p256PublicPKCS8)
+      })
+
+      it('should convert to publicJwk', () => {
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(p256PublicJwk)
+      })
+
+      it('should not convert to privatePKCS1', () => {
+        expect(() => key.toString('pem', 'private_pkcs1')).to.throw('Cannot export a private key from a public key')
+      })
+
+      it('should not convert to privatePKCS8', () => {
+        expect(() => key.toString('pem', 'private_pkcs8')).to.throw('Cannot export a private key from a public key')
+      })
+
+      it('should not convert to privateJwk', () => {
+        expect(() => JSON.stringify(key.toJwk('private'), null, 2)).to.throw('Cannot export a private key from a public key')
+      })
+    })
+
+    describe('publicJwk', () => {
+
+      let key
+      before(() => {
+        key = keyto.from(p256PublicJwk, 'jwk')
+      })
+
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
+
+      it('should convert to publicPKCS8', () => {
+        key.toString('pem', 'public_pkcs8').should.equal(p256PublicPKCS8)
+      })
+
+      it('should convert to publicJwk', () => {
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(p256PublicJwk)
+      })
+
+      it('should not convert to privatePKCS1', () => {
+        expect(() => key.toString('pem', 'private_pkcs1')).to.throw('Cannot export a private key from a public key')
+      })
+
+      it('should not convert to privatePKCS8', () => {
+        expect(() => key.toString('pem', 'private_pkcs8')).to.throw('Cannot export a private key from a public key')
+      })
+
+      it('should not convert to privateJwk', () => {
+        expect(() => JSON.stringify(key.toJwk('private'), null, 2)).to.throw('Cannot export a private key from a public key')
+      })
+    })
+
+    describe('privatePKCS1', () => {
+
+      let key
+      before(() => {
+        key = keyto.from(p256PrivatePKCS1, 'pem')
+      })
+
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
+
+      it('should convert to publicPKCS8', () => {
+        key.toString('pem', 'public_pkcs8').should.equal(p256PublicPKCS8)
+      })
+
+      it('should convert to publicJwk', () => {
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(p256PublicJwk)
+      })
+
+      it('should convert to privatePKCS1', () => {
+        key.toString('pem', 'private_pkcs1').should.equal(p256PrivatePKCS1)
+      })
+
+      it('should convert to privatePKCS8', () => {
+        key.toString('pem', 'private_pkcs8').should.equal(p256PrivatePKCS8)
+      })
+
+      it('should convert to privateJwk', () => {
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(p256PrivateJwk)
+      })
+    })
+
+    describe('privatePKCS8', () => {
+
+      let key
+      before(() => {
+        key = keyto.from(p256PrivatePKCS8, 'pem')
+      })
+
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
+
+      it('should convert to publicPKCS8', () => {
+        key.toString('pem', 'public_pkcs8').should.equal(p256PublicPKCS8)
+      })
+
+      it('should convert to publicJwk', () => {
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(p256PublicJwk)
+      })
+
+      it('should convert to privatePKCS1', () => {
+        key.toString('pem', 'private_pkcs1').should.equal(p256PrivatePKCS1)
+      })
+
+      it('should convert to privatePKCS8', () => {
+        key.toString('pem', 'private_pkcs8').should.equal(p256PrivatePKCS8)
+      })
+
+      it('should convert to privateJwk', () => {
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(p256PrivateJwk)
+      })
+    })
+
+    describe('privateJwk', () => {
+
+      let key
+      before(() => {
+        key = keyto.from(p256PrivateJwk, 'jwk')
+      })
+
+      it('should convert to publicPKCS1', () => {
+        expect(() => key.toString('pem', 'public_pkcs1')).to.throw('This has not been implemented yet')
+      })
+
+      it('should convert to publicPKCS8', () => {
+        key.toString('pem', 'public_pkcs8').should.equal(p256PublicPKCS8)
+      })
+
+      it('should convert to publicJwk', () => {
+        JSON.stringify(key.toJwk('public'), null, 2).should.equal(p256PublicJwk)
+      })
+
+      it('should convert to privatePKCS1', () => {
+        key.toString('pem', 'private_pkcs1').should.equal(p256PrivatePKCS1)
+      })
+
+      it('should convert to privatePKCS8', () => {
+        key.toString('pem', 'private_pkcs8').should.equal(p256PrivatePKCS8)
+      })
+
+      it('should convert to privateJwk', () => {
+        JSON.stringify(key.toJwk('private'), null, 2).should.equal(p256PrivateJwk)
       })
     })
   })
